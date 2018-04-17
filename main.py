c82151e2315e8318871865f1d50a1ccd50b0d85f
W-KE <wangke97@outlook.com>
2018-04-17 22:17:05 +1200

    Update main.py

--- main.py
+++ main.py
@@ -91,16 +91,8 @@ async def poll(ctx, question, *options: str):
     if len(options) > 10:
         await bot.say('You cannot make a poll for more than 10 things!')
         return
-
-    if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
-        reactions = ['✅', '❌']
-    else:
-        reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
-        reactions = emoji
-    description = []
-    for x, option in enumerate(options):
-        description += '\n {} {}'.format(reactions[x], option)
-    embed = discord.Embed(title=question, description=''.join(description))
+    reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
+    embed = discord.Embed(title=question, description="")
     react_message = await bot.say(embed=embed)
     for reaction in reactions[:len(options)]:
         await bot.add_reaction(react_message, reaction)
